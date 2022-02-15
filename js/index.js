cf67f3ac3d70eeb7c9c394eccce306d1b7d89dbd
sheharyark017 <sheharyark017@gmail.com>
2022-02-15 19:29:16 +0500

    second commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
+// declarations
+
 const accordions = document.querySelectorAll(".accordion-block");
 
 const nav = document.querySelector(".main-nav");
+
+const innerNav = document.querySelector(".inner-nav");
 
 const navHeight = nav.clientHeight;
 
 const navLinks = document.querySelector(".nav-links");
 
 const hamburger = document.querySelector(".hamburger");
+
+const cartButton = document.querySelector(".cart-button");
+
+const cart = document.querySelector(".cart-sidebar");
+
+const cartCloseButton = document.querySelector(".cart-close");
+
+const homeBody = document.querySelector(".home-body");
+
+// end declarations
+
+// accordions
 
 for (i = 0; i < accordions.length; i++) {
   accordions[i].addEventListener("click", function () {
@@ -18,6 +34,10 @@ for (i = 0; i < accordions.length; i++) {
   });
 }
 
+// end accordions
+
+// phone navbar
+
 navLinks.style.top = navHeight - 30 + "px";
 
 hamburger.addEventListener("click", function () {
@@ -25,3 +45,22 @@ hamburger.addEventListener("click", function () {
 
   navLinks.classList.toggle("nav-links-acive");
 });
+
+// end phone navbar
+
+// moving cart
+
+cartButton.addEventListener("click", function () {
+  cart.classList.add("cart-active");
+});
+
+cartCloseButton.addEventListener("click", function () {
+  console.log("hello");
+  cart.classList.remove("cart-active");
+});
+
+homeBody.addEventListener("click", function () {
+  cart.classList.remove("cart-active");
+});
+
+// end moving cart
